3b38a761d6a1cab28469f130ef1a1d101f48b5e7
cheaps-k <cheaps.kk@gmail.com>
2020-04-23 23:07:21 +0900

    filter／reload機能を追加

--- rolling_stock/www/js/index.js
+++ rolling_stock/www/js/index.js
@@ -16,6 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+let gIsFiltered = false;
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -29,44 +32,9 @@ var app = {
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
         
-        let request_url = `${REDMINE.url}/projects/${REDMINE.project_name}/issues.json?key=${REDMINE.api_key}&limit=100&sort=id`;
-        
-        $.ajax({
-            url: request_url,
-            type: "GET",
-            dataType: 'json'
-        }).done(function(data, status, xhr) {
-            let items = document.getElementById("items");
-            let template = document.getElementById("item_template");
-            
-            for(let issue of data.issues) {
-                let item_id = issue.id;
-                let item_name = issue.subject;
-                let required_num;
-                let stocked_num;
-                for( cf of issue.custom_fields ) {
-                    if( cf.id == REDMINE.custom_field_id.required_num ) {
-                        required_num = Number( cf.value );
-                    } else if( cf.id == REDMINE.custom_field_id.stocked_num ) {
-                        stocked_num = Number( cf.value );
-                    }
-                }
-                let defective_num = calcDefectiveNum( required_num, stocked_num );
-                
-                let add_item = template.cloneNode(true);
-                
-                add_item.id = item_id;
-                add_item.getElementsByClassName("item_name")[0].innerHTML = item_name;
-                add_item.getElementsByClassName("item_required_num_val")[0].innerHTML = String( required_num );
-                add_item.getElementsByClassName("item_stocked_num_val")[0].innerHTML = String( stocked_num );
-                add_item.getElementsByClassName("item_defective_num_val")[0].innerHTML = String( defective_num );
-                add_item.getElementsByClassName("item_dec")[0].addEventListener("click", { item_id: item_id, handleEvent: itemDecriment });
-                add_item.getElementsByClassName("item_inc")[0].addEventListener("click", { item_id: item_id, handleEvent: itemIncriment });
-                items.appendChild(add_item); 
-            }
-        }).fail(function(xhr, status, error) {
-            console.log( {"xhr": xhr, "status": status, "error": error } );
-        });
+        reloadItems();
+        document.getElementById("filter").addEventListener("click", clickedFilter);
+        document.getElementById("reload").addEventListener("click", clickedReload);
     },
 
     // Update DOM on a Received Event
@@ -74,6 +42,92 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+function clickedFilter( e ) {
+    let items = document.getElementById("items");
+    let defective_num;
+    
+    if( gIsFiltered == false ) {
+        // Set the filter
+        
+        for( target_item of items.children ) {
+            defective_num = Number( target_item.getElementsByClassName("item_defective_num_val")[0].innerHTML );
+            if( defective_num == 0 ) {
+                target_item.style.display = "none";
+            }
+        }
+        gIsFiltered = true;
+    } else {
+        // Remove the filter
+        
+        for( target_item of items.children ) {
+            target_item.style.display = "flex";
+        }
+        gIsFiltered = false;
+    }
+}
+
+function clickedReload( e ) {
+    reloadItems();
+}
+
+function reloadItems() {
+    let items = document.getElementById("items");
+    let reloading_img = document.getElementById("reloading_img");
+    
+    // Clear items and display reloading image
+    items.innerHTML = "";
+    items.style.display = "none";
+    reloading_img.style.display = "block";
+    
+    let request_url = `${REDMINE.url}/projects/${REDMINE.project_name}/issues.json?key=${REDMINE.api_key}&limit=100&sort=id`;
+    
+    $.ajax({
+        url: request_url,
+        type: "GET",
+        dataType: 'json'
+    }).done(function(data, status, xhr) {
+        let template = document.getElementById("item_template").children[0];
+        
+        items.innerHTML = "";
+        for(let issue of data.issues) {
+            let item_id = issue.id;
+            let item_name = issue.subject;
+            let required_num;
+            let stocked_num;
+            for( cf of issue.custom_fields ) {
+                if( cf.id == REDMINE.custom_field_id.required_num ) {
+                    required_num = Number( cf.value );
+                } else if( cf.id == REDMINE.custom_field_id.stocked_num ) {
+                    stocked_num = Number( cf.value );
+                }
+            }
+            let defective_num = calcDefectiveNum( required_num, stocked_num );
+            
+            let add_item = template.cloneNode(true);
+            
+            add_item.id = item_id;
+            add_item.getElementsByClassName("item_name")[0].innerHTML = item_name;
+            add_item.getElementsByClassName("item_required_num_val")[0].innerHTML = String( required_num );
+            add_item.getElementsByClassName("item_stocked_num_val")[0].innerHTML = String( stocked_num );
+            add_item.getElementsByClassName("item_defective_num_val")[0].innerHTML = String( defective_num );
+            add_item.getElementsByClassName("item_dec")[0].addEventListener("click", { item_id: item_id, handleEvent: itemDecriment });
+            add_item.getElementsByClassName("item_inc")[0].addEventListener("click", { item_id: item_id, handleEvent: itemIncriment });
+            items.appendChild(add_item); 
+        }
+
+        reloading_img.style.display = "none";
+        items.style.display = "block";
+        document.getElementById("items").scrollTop = 0;
+        
+        gIsFiltered = false;
+        
+    }).fail(function(xhr, status, error) {
+        console.log( {"xhr": xhr, "status": status, "error": error } );
+        reloading_img.style.display = "none";
+        errorOccurred( `ストック情報取得エラー(${xhr.status})` );
+    });
+}
 
 function itemDecriment( e ) {
     changeItemNum( this.item_id, function( val ) { return ( val - 1 ); } );
@@ -95,17 +149,34 @@ function changeItemNum( item_id, calculator ) {
     let defective_num = calcDefectiveNum( required_num, stocked_num );
     
     // Update server
-    let request_url = `${REDMINE.url}/issues/${item_id}.json?key=${REDMINE.api_key}`;
+    updateTicketStockNum( item_id, stocked_num );
+
+    // Update display
+    elem_required_num.innerHTML = required_num;
+    elem_stocked_num.innerHTML = stocked_num;
+    elem_defective_num.innerHTML = defective_num;
+    
+    if( gIsFiltered == true ) {
+        if( defective_num == 0 ) {
+            elem_item.style.display = "none";
+        }
+    }
+}
+
+function updateTicketStockNum( ticket_id, new_stock_num ) {
+    let request_url = `${REDMINE.url}/issues/${ticket_id}.json?key=${REDMINE.api_key}`;
+    
     update_data = {
         "issue": {
             "custom_fields": [
                 {
                     "id": REDMINE.custom_field_id.stocked_num,
-                    "value": String( stocked_num )
+                    "value": String( new_stock_num )
                 }
             ]
         }
     };
+    
     $.ajax({
         url: request_url,
         type: "PUT",
@@ -113,12 +184,10 @@ function changeItemNum( item_id, calculator ) {
         data: JSON.stringify(update_data),
         dataType: 'text'
     }).done(function(data, status, xhr) {
-        // Update display
-        elem_required_num.innerHTML = required_num;
-        elem_stocked_num.innerHTML = stocked_num;
-        elem_defective_num.innerHTML = defective_num;
+        // Do nothing
     }).fail(function(xhr, status, error) {
         console.log( {"xhr": xhr, "status": status, "error": error } );
+        errorOccurred( `ストック情報更新エラー(${xhr.status})` );
     });
 }
 
@@ -129,6 +198,11 @@ function calcDefectiveNum( required_num, stocked_num ) {
     }
     
     return defective_num;
+}
+
+function errorOccurred( message ) {
+    alert( message );
+    document.getElementById("items").innerHTML = "";
 }
 
 app.initialize();
